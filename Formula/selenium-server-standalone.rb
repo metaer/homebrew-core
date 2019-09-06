192aeb883c9efb1a7060e89f904b2073e27b29e6
metaer <pavelpopovw@gmail.com>
2019-09-06 14:22:43 +0300

    Update selenium-server-standalone.rb

--- Formula/selenium-server-standalone.rb
+++ Formula/selenium-server-standalone.rb
@@ -1,7 +1,7 @@
 class SeleniumServerStandalone < Formula
   desc "Browser automation for testing purposes"
   homepage "https://www.seleniumhq.org/"
-  url "https://selenium-release.storage.googleapis.com/3.141/selenium-server-standalone-3.141.59.jar"
+  url "http://selenium-release.storage.googleapis.com/2.52/selenium-server-standalone-2.52.0.jar"
   sha256 "acf71b77d1b66b55db6fb0bed6d8bae2bbd481311bcbedfeff472c0d15e8f3cb"
 
   bottle :unneeded
